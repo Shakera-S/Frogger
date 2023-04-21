eb0378704cd21d2e4cf93dc8749216c9c251d18d
Shakera Shephard <Scarlet.Scar12@gmail.com>
2023-04-21 19:01:52 -0400

    Updates

--- app.js
+++ app.js
@@ -1,42 +1,54 @@
 const timeLeftDisplay = document.querySelector("#time-left");
 const resultDisplay = document.querySelector("#result");
-const startPauseButton = document.querySelector("#startpause");
-const logsLeft = document.querySelectorAll(".log-left");
-
+const startPauseButton = document.querySelector("#start-pause-button");
 const squares = document.querySelectorAll(".grid div");
+const logsLeft = document.querySelectorAll(".log-left");
 console.log(squares);
+
 let currentIndex = 76;
-const width = 9
+const width = 9;
 
-// Creating our green frog and give it the ability to move
 function moveFrog(e) {
-    squares[currentIndex].classList.remove("frog"); // This is to prevent the frog from leaving a trail
+  squares[currentIndex].classList.remove("frog");
 
-    switch(e.key) {
-        case "w" :
-            if ( currentIndex - width >= 0) currentIndex -= width
-            break
-        case "s" :
-            if (currentIndex + width < width * width) currentIndex += width
-            break
-        case "d" :
-            if (currentIndex % width < width - 1) currentIndex += 1
-            break
-        case "a" :
-            if (currentIndex % width !== 0) currentIndex -= 1
-            break
-    }
-
-    squares[currentIndex].classList.add("frog")
+  switch (e.key) {
+    case "ArrowLeft":
+      if (currentIndex % width !== 0) currentIndex -= 1;
+      break;
+    case "ArrowRight":
+      if (currentIndex % width < width - 1) currentIndex += 1;
+      break;
+    case "ArrowUp":
+      if (currentIndex - width >= 0) currentIndex -= width;
+      break;
+    case "ArrowDown":
+      if (currentIndex + width < width * width) currentIndex += width;
+      break;
+  }
+  squares[currentIndex].classList.add("frog");
 }
 document.addEventListener("keyup", moveFrog);
 
 function autoMoveLogs() {
-    logsLeft.forEach
+  logsLeft.forEach((logLeft) => moveLogLeft(logLeft));
+}
+autoMoveLogs;
+
+function moveLogLeft(logLeft) {
+  switch (true) {
+    case logLeft.classList.contains("L1"):
+      logLeft.classList.remove("L1");
+      logLeft.classList.add("L2");
+      break;
+    case logLeft.classList.contains("L2"):
+      logLeft.classList.remove("L2");
+      logLeft.classList.add("L3");
+      break;
+    case logLeft.classList.contains("L3"):
+      logLeft.classList.remove("L3");
+      logLeft.classList.add("L4");
+      break;
+  }
 }
 
-// Get the cars and logs moving
-function moveLogLeft() {
-    switch(true)
-    case
-}
+setInterval(autoMoveLogs, 1000);
